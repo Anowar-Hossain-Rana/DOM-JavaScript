8c7ba402baef60a876e16a8f60a5b39f3c656e86
Anowar-Hossain-Rana <anowar.agrobd@gmail.com>
2022-08-13 21:43:58 +0600

    fast

---
+++ practice.js
@@ -0,0 +1,9 @@
+
+
+// getElementByID
+
+// document.getElementById("hadding1").innerHTML = "Hello";
+
+// document.getElementById('para').innerHTML = 'bye';
+
+// document.getElementById('hadding2').innerHTML='Good Bye Good By';
